0f6f5727dbd286716f048d6578aa91809fbcdcd6
slicenbeat <slicenbeat@gmail.com>
2022-10-10 12:40:22 +0400

    feat: add async repository action

---
+++ src/asyncActions/repository.js
@@ -0,0 +1,20 @@
+import { getRepositoryAction } from "../store/repositoryReducer";
+import axios from "axios";
+
+export const fetchRepo = (repoUser) => {
+  return function (dispatch) {
+    axios
+      .get(`https://api.github.com/repos/${repoUser}/commits`)
+      .then((response) => {
+        getRepositoryAction(
+          response.map((repository) => {
+            return {
+              authorName: repository.commit.author.name,
+              sha: repository.sha,
+              date: repository.commit.date.slice(0, 10),
+            };
+          })
+        );
+      });
+  };
+};
